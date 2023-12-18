b7e32ddccdc7cc5aaec6fe2ea817fc20806482ac
appsec.andrewlee <appsec.andrewlee@gmail.com>
2023-12-18 15:54:47 +1100

    added all dependencies and working on MongoCred

--- showcase/src/main/java/etherealcv/showcase/ShowcaseApplication.java
+++ showcase/src/main/java/etherealcv/showcase/ShowcaseApplication.java
@@ -1,13 +1,17 @@
 package etherealcv.showcase;
-
+import connectiondriver.MongoCred;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ShowcaseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShowcaseApplication.class, args);
 	}
 
+
+	MongoCred mongoCred;
+
 }
+
